42f6065ee71a49902448f7ca70d4d8b58cb9990c
DeManoj <mano.tamang12@outlook.com>
2026-06-14 17:46:18 +0545

    Happy Birthda babe

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import './index.css'
-import App from './App.tsx'
+import { HashRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
